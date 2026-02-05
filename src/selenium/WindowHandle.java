dd442c56cfb54a0d2689f10ff6c85563f117e9da
Jagadheswaran <jagadhesjack88@gmail.com>
2026-02-06 00:43:42 +0530

    Updated Window Handling

--- src/selenium/WindowHandle.java
+++ src/selenium/WindowHandle.java
@@ -1,9 +1,13 @@
 package selenium;
 
+import java.time.Duration;
 import java.util.Set;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -17,20 +21,41 @@ public class WindowHandle {
 		driver.manage().window().maximize();
 
 		// Load the website
-		driver.get("http://www.naukri.com/");
+		driver.get("https://testautomationpractice.blogspot.com/");
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
 		// It will return the parent window name as a String
-		String parent=driver.getWindowHandle();
-		Set<String>s=driver.getWindowHandles();
-		for (String child_window : s) {
-			if(!parent.equals(child_window))
-			{
-				driver.switchTo().window(child_window);
-				System.out.println(driver.switchTo().window(child_window).getTitle());
-				driver.close();
-			}
+		String parent = driver.getWindowHandle();
+		
+		Actions action = new Actions(driver);
+		action.moveToElement(driver.findElement(By.id("PopUp")));
+		
+		WebElement openWindows = driver.findElement(By.id("PopUp"));
+		openWindows.click();
+		
+		Set<String> windows = driver.getWindowHandles();
+
+		for (String win : windows) {
+		    driver.switchTo().window(win);
+		    System.out.println(driver.getTitle());
 		}
-		//switch to the parent window
+
+		// Close child windows only
+		for (String win : windows) {
+		    if (!win.equals(parent)) {
+		        driver.switchTo().window(win);
+		        driver.close();
+		    }
+		}
+
+		// Go back to parent
 		driver.switchTo().window(parent);
 
+		// Small wait to let WebSocket finish
+		Thread.sleep(1000);
+
+		// Close everything cleanly
+		driver.quit();
 	}
 }
